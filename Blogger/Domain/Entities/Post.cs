937f0e6ecd1ed128ff1c8da23ba9c65cefd5b7b7
ArsySoriah <szkolnikcb@gmail.com>
2021-12-16 11:27:37 +0100

    first commit

---
+++ Blogger/Domain/Entities/Post.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Domain.Common;
+
+
+namespace Domain.Entities
+{
+    public class Post : AuditableEntity
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        //konstruktor bezparametrowy
+        public Post()
+        {
+        }
+
+        //konstruktor parametrowy
+        public Post(int id, string title, string content)
+        {
+            (Id, Title, Content) = (id, title, content);
+        }
+    }
+}
